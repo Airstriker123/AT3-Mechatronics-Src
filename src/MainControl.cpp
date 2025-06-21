3552301667a6881e2524ba30f38f080a71eff498
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2025-06-21 23:57:10 +1000

    Src uploaded basic no impl
    main?

---
+++ src/MainControl.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+namespace MainControl {
+    class Mechalogic {
+    public:
+        void setup() {
+            // write your initialization code here
+        }
+
+        void loop() {
+            // write your code here
+        }
+    private:
+        // private functions
+    };
+};
+
+
+
+
+
+
