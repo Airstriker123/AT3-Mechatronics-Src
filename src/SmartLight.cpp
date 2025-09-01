ab4a0733f3f090991b6017e2b447602f268106ce
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2025-09-01 20:42:37 +1000

    Added comments and completed code
    ez
    (hope it works)

--- src/SmartLight.cpp
+++ src/SmartLight.cpp
@@ -1,6 +1,4 @@
-#include <any>
 #include <Arduino.h>
-#include <print.h>
 
 namespace SmartLight
 {
@@ -17,14 +15,17 @@ namespace SmartLight
         bool MotionSensorStateOn;
         bool LightSensorStateOn;
         bool StartSmartLight;
+        int LedPin;
         const int PhotoResistorpin; //define the pin
         const int MotionSensorPin; //  define the pin
         int Motionstate; //define pin = 0
 
 
         SmartLight() //constructor
-        {
+        //sensor pins
+            : PhotoResistorpin(A1), MotionSensorPin(A1) {
             //predefine variables
+            LedPin = A1;
             StartSmartLight = false;
             MotionSensorStateOn = true;
             LightSensorStateOn = true;
@@ -39,7 +40,19 @@ namespace SmartLight
 
         bool LightSensor()
         /*
+BEGIN LightSensor
+    \\ process to request light from sensor and determine if light is on or off
+    READ LightLevel FROM sensor  \\ sensor will return light intensity
 
+    \\ determine if to mark as light detected or not to avoid false positive
+    IF LightLevel > 150 THEN
+       RETURN  Light = True
+       write "Sensor is detecting light!"
+    ELSE
+       RETURN  Light = False
+       write "No light detected"
+    ENDIF
+END LightSensor
 */
         {
             try {
@@ -58,37 +71,73 @@ namespace SmartLight
             catch (...) {
                 // incase sensor fails to notify me to fix it!
                 Serial.println("Light Sensor could not be detected");
-                return LightSensorStateOn = false;
+                return LightSensorStateOn = false; //flag for deciding if smartlight should proceed
             }
             return Light = false;
         }
 
           bool MotionSensor()
-        /*
+          /*
+BEGIN MotionSensor
+  \\ process to perform motion check
+  READ Motion from sensor
 
-         */
+  IF Motion THEN
+      RETURN  Motion = True
+      write "Motion detected!"
+  ELSE
+      RETURN  Motion = False
+      write "No motion detected"
+  ENDIF
+END MotionSensor
+*/
         {
             try {
                 delay(1000);
                 Motionstate = digitalRead(MotionSensorPin);         // Read the state of the PIR sensor
                 if (Motionstate == HIGH) {                 // If the PIR sensor detects movement (state = HIGH)
-                    return Motion = true;
+                    return Motion = true; //flag for deciding if light should be on/off
                     Serial.print("Motion is detecting motion!");
                 }
-                return Motion = false;
+                return Motion = false; //flag for deciding if light should be on/off
                 Serial.println("Monitoring...");
             } catch (...) { //incase sensor fails to notify me to fix it!
-                return MotionSensorStateOn = false;
+                return MotionSensorStateOn = false; //flag for deciding if smartlight should proceed
                 Serial.println("Motion Sensor could not be detected");
             }
-            return Motion = false;
+            return Motion = false; //flag for deciding if light should be on/off
         }
 
          bool setup()
-            /*
+        /*
+\\ Ardunio setup process only executes once
+BEGIN setup
+     \\ safety checks to check if product sensors are working
+     READ MotionSensorStatus FROM sensor
+     READ LightSensorStatus FROM sensor
 
-             */
+     IF MotionSensorStatus = True THEN
+         Sensor1 = True
+         write "Motion sensor detected!"
+     ELSE
+         write "Motion sensor not detected!"
+     ENDIF
+
+     IF LightSensorStatus = True THEN
+         Sensor2 = True
+         write "Light sensor detected!"
+     ELSE
+         write "Light sensor not detected!"
+     ENDIF
+
+     IF Sensor1 AND Sensor2 = True THEN
+         RETURN  StartSmartLight = True
+         write "Smart Light is now active!"
+     ENDIF
+END setup
+*/
         {
+            pinMode(LedPin, OUTPUT); //led location (readjust if needed)
             pinMode(MotionSensorPin, INPUT);  // Set the PIR pin as an input
             Serial.begin(9600);
             Serial.println("initializing SmartLight setup!");
@@ -117,22 +166,30 @@ namespace SmartLight
         void loop()
         /*
 \\ process to perform real time checks if any change in environment
-BRGIN loop
+BEGIN loop
     IF StartSmartLight = True THEN \\ only start if all sensors work
        IF MOTION AND LIGHT = True THEN
           write " LED light on!"
-          SmartLight = True
           LEDLight = On
        ELSE
            write "LED light off"
-           SmartLight = False
            LEDLight = off
        ENDIF
     ENDIF
 END Loop
 */
         {
-            // write your code here
+            if (StartSmartLight == true) { //only start if sensors work
+                if (Light == true && Motion == true ) { //check if both sensors are detecting
+                    //turn light on if they are
+                    Serial.println("LED light on!");
+                    digitalWrite(LedPin, HIGH);
+                }
+                else { //turn light off if not
+                    Serial.println("LED light off!");
+                    digitalWrite(LedPin, LOW);
+                }
+            }
         }
     };
 };
