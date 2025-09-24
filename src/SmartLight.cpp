26e12878832082a06d7ea7badd63e333f720d985
Airstriker <amt.sngh771@gmail.com>
2025-09-24 14:44:22 +1000

    Created a md development file of the guessing game
    - added source code
    - explaining some of the code

--- src/SmartLight.cpp
+++ src/SmartLight.cpp
@@ -39,10 +39,10 @@ BEGIN LightSensor
     \\ determine if to mark as light detected or not to avoid false positive
     IF LightLevel > 150 THEN
        RETURN  Light = True
-       write "Sensor is detecting light!"
+       Display "Sensor is detecting light!"
     ELSE
        RETURN  Light = False
-       write "No light detected"
+       Display "No light detected"
     ENDIF
 END LightSensor
 */
@@ -69,21 +69,21 @@ BEGIN setup
 
      IF MotionSensorStatus = True THEN
          Sensor1 = True
-         write "Motion sensor detected!"
+         Display "Motion sensor detected!"
      ELSE
-         write "Motion sensor not detected!"
+         Display "Motion sensor not detected!"
      ENDIF
 
      IF LightSensorStatus = True THEN
          Sensor2 = True
-         write "Light sensor detected!"
+         Display "Light sensor detected!"
      ELSE
-         write "Light sensor not detected!"
+         Display "Light sensor not detected!"
      ENDIF
 
      IF Sensor1 AND Sensor2 = True THEN
          RETURN  StartSmartLight = True
-         write "Smart Light is now active!"
+         Display "Smart Light is now active!"
      ENDIF
 END setup
 */
@@ -113,10 +113,10 @@ END setup
 BEGIN loop
     IF StartSmartLight = True THEN \\ only start if all sensors work
        IF Motion AND Light = True THEN
-          write " LED light on!"
+          Display " LED light on!"
           LEDLight = On
        ELSE
-           write "LED light off"
+           Display "LED light off"
            LEDLight = off
        ENDIF
     ENDIF
@@ -128,10 +128,10 @@ END Loop
 
                 if (Light == false) {
                     Serial.println("LED ON");
-                    digitalWrite(LedPin, HIGH);
+                    digitalDisplay(LedPin, HIGH);
                 } else {
                     Serial.println("LED OFF");
-                    digitalWrite(LedPin, LOW);
+                    digitalDisplay(LedPin, LOW);
                 }
             }
             delay(1500);
@@ -144,3 +144,87 @@ SmartLight::SmartLight SmartLightSystem;
 //for compiling method
 void setup() {SmartLightSystem.setup();};
 void loop() {SmartLightSystem.loop();};
+
+
+
+/*
+LEDLight = off
+LightLevel = 0
+Light = False
+Motion = False
+
+BEGIN PhotoResistor
+    \\ process to request light from sensor and determine if light is on or off
+    LightLevel = READ LightLevel FROM sensor  \\ sensor will return light intensity
+
+    \\ determine if to mark as light detected or not to avoid false positive
+    IF LightLevel > 150 THEN
+       RETURN  Light = True
+       Display "Sensor is detecting light!"
+    ELSE
+       RETURN  Light = False
+       Display "No light detected"
+    ENDIF
+END PhotoResistor
+
+
+BEGIN MotionSensor
+    \\ process to perform motion check
+    READ Motion from sensor
+
+    IF Motion THEN
+        RETURN  Motion = True
+        Display "Motion detected!"
+    ELSE
+        RETURN  Motion = False
+        Display "No motion detected"
+    ENDIF
+END MotionSensor
+
+\\ Ardunio setup process only executes once
+BEGIN setup
+     \\ safety checks to check if product sensors are working
+     MotionSensorStatus = get from sensor
+     LightSensorStatus = get from sensor 
+
+     IF MotionSensorStatus = True THEN
+         Sensor1 = True
+         Display "Motion sensor detected!"
+     ELSE
+         Display "Motion sensor not detected!"
+     ENDIF
+
+     IF LightSensorStatus = True THEN
+         Sensor2 = True
+         Display "Light sensor detected!"
+     ELSE
+         Display "Light sensor not detected!"
+     ENDIF
+
+     IF Sensor1 AND Sensor2 = True THEN
+         Display "Smart Light is now active!"
+RETURN  StartSmartLight = True	
+     ENDIF
+END setup
+
+
+
+
+\\ process to perform real time checks if any change in environment
+BEGIN loop
+    IF StartSmartLight = True THEN \\ only start if all sensors work
+       IF MOTION AND LIGHT = True THEN
+          Display " LED light on!"
+          LEDLight = On
+       ELSE
+           Display "LED light off"
+           LEDLight = off
+       ENDIF
+    ENDIF
+END Loop
+
+
+
+
+
+ */
